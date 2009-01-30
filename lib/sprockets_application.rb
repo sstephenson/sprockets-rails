5c07d8a362608063444dc9ff08a18237e4f6b95c
Sam Stephenson <sam@37signals.com>
2009-01-29 22:01:35 -0600

    Make use of Sprockets::Secretary

--- lib/sprockets_application.rb
+++ lib/sprockets_application.rb
@@ -4,10 +4,6 @@ module SprocketsApplication
       map.resource(:sprockets)
     end
     
-    def output_file
-      preprocessor_for(configuration).output_file
-    end
-
     def source
       output_file.to_s
     end
@@ -16,34 +12,21 @@ module SprocketsApplication
       output_file.save_to(asset_path)
     end
 
-    def asset_path
-      File.join(Rails.public_path, "sprockets.js")
-    end
-
     protected
+      def secretary
+        Sprockets::Secretary.new(configuration.merge(:root => RAILS_ROOT))
+      end
+    
       def configuration
         YAML.load(IO.read(File.join(RAILS_ROOT, "config", "sprockets.yml"))) || {}
       end
-    
-      def expand_paths(paths)
-        paths.map { |path| Dir[path] }.flatten.compact
-      end
-      
-      def preprocessor_for(configruation)
-        environment = environment_for(configuration)
-        returning Sprockets::Preprocessor.new(environment) do |preprocessor|
-          expand_paths(configuration[:source_files]).each do |filename|
-            if pathname = environment.find(filename)
-              preprocessor.require(pathname.source_file)
-            else
-              raise Sprockets::LoadError, "no such file `#{filename}'"
-            end
-          end
-        end
+
+      def output_file
+        secretary.output_file
       end
 
-      def environment_for(configuration)
-        Sprockets::Environment.new(RAILS_ROOT, expand_paths(configuration[:load_path]))
+      def asset_path
+        File.join(Rails.public_path, "sprockets.js")
       end
   end
 end
