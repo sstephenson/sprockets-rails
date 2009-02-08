e3603df6061b410bae55e964384f11d90a4b9dd3
Sam Stephenson <sam@37signals.com>
2009-02-08 12:30:15 -0600

    Update install.rb to mkdir -p vendor/sprockets app/javascripts and touch app/javascripts/application.js

--- install.rb
+++ install.rb
@@ -1,4 +1,10 @@
 require "fileutils"
+include FileUtils::Verbose
 
-FileUtils.cp File.join(File.dirname(__FILE__), "config", "sprockets.yml"),
-             File.join(File.dirname(__FILE__), "..", "..", "..", "config")
+RAILS_ROOT = File.join(File.dirname(__FILE__), "..", "..", "..") unless defined?(RAILS_ROOT)
+
+mkdir_p File.join(RAILS_ROOT, "vendor", "sprockets")
+mkdir_p File.join(RAILS_ROOT, "app", "javascripts")
+touch   File.join(RAILS_ROOT, "app", "javascripts", "application.js")
+cp      File.join(File.dirname(__FILE__), "config", "sprockets.yml"), 
+        File.join(RAILS_ROOT, "config")
